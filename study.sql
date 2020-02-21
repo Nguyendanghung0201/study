08e9562c18953055de883945a8ddfc39578ebc50
unknown <Flash>
2020-02-21 17:44:03 +0700

    commit oksql

--- study.sql
+++ study.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2020 at 10:08 AM
+-- Generation Time: Feb 21, 2020 at 11:43 AM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -25,23 +25,239 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ask`
+--
+
+CREATE TABLE `ask` (
+  `id` int(11) NOT NULL,
+  `username_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `content` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `grade` int(255) NOT NULL,
+  `subject` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chemistry`
+--
+
+CREATE TABLE `chemistry` (
+  `id` int(11) NOT NULL,
+  `content` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `answer` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `grade` int(255) NOT NULL,
+  `lever` int(255) NOT NULL,
+  `classy` int(255) NOT NULL,
+  `categories` int(255) NOT NULL,
+  `method` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `chemistry`
+--
+
+INSERT INTO `chemistry` (`id`, `content`, `answer`, `grade`, `lever`, `classy`, `categories`, `method`) VALUES
+(0, 'Đốt cháy hoàn toàn m gam triglixerit X cần vừa đủ 3,22 mol O2 thu được CO2 và 2,12 mol H2O. Mặt khác, cho m gam X tác dụng vừa đủ NaOH thu được a gam hỗn hợp hai muối natri stearat và natri oleat. Giá trị của a là', '33,36', 12, 1, 1, 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choicecchemistry`
+--
+
+CREATE TABLE `choicecchemistry` (
+  `id` int(11) NOT NULL,
+  `ques_id` int(11) NOT NULL,
+  `choose` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `choicecchemistry`
+--
+
+INSERT INTO `choicecchemistry` (`id`, `ques_id`, `choose`) VALUES
+(1, 0, '33,36'),
+(2, 0, '36,56'),
+(3, 0, '34,96 '),
+(4, 0, '35,44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clan`
+--
+
+CREATE TABLE `clan` (
+  `nameclan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `point` int(11) NOT NULL,
+  `rankclan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `ask_id` int(11) NOT NULL,
+  `username_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE `exam` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `point` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `ques` int(11) NOT NULL,
+  `result` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(255) NOT NULL,
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+  `id` int(11) NOT NULL,
+  `grade` float DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `point` int(255) NOT NULL,
+  `gold` int(255) NOT NULL,
+  `school` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `rank` int(255) NOT NULL,
+  `clan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `avatar` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `phone`) VALUES
-(0, 'danghung', 'danghung0201@gmail.com', 'Danghung0301$', '0914339534');
+--
+-- Indexes for table `ask`
+--
+ALTER TABLE `ask`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `chemistry`
+--
+ALTER TABLE `chemistry`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `choicecchemistry`
+--
+ALTER TABLE `choicecchemistry`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clan`
+--
+ALTER TABLE `clan`
+  ADD PRIMARY KEY (`nameclan`),
+  ADD UNIQUE KEY `nameclan` (`nameclan`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`,`username`),
+  ADD KEY `users_fk0` (`clan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ask`
+--
+ALTER TABLE `ask`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `choicecchemistry`
+--
+ALTER TABLE `choicecchemistry`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `exam`
+--
+ALTER TABLE `exam`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_fk0` FOREIGN KEY (`clan`) REFERENCES `clan` (`nameclan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
